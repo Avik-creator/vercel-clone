6479c096c3e340d36809e0aec142d92d043c1b8b
Avik-creator <avikm744@gmail.com>
2026-05-16 21:57:46 +0530

    Added DB connections code using SQLX

---
+++ src/db.rs
@@ -0,0 +1,34 @@
+use sqlx::{postgres::PgPoolOptions, PgPool};
+
+#[derive(Clone, Debug)]
+pub struct Database {
+    pub pool: PgPool,
+}
+
+impl Database {
+  pub async fn connect(url: &str) -> Result<Self, sqlx::Error> {
+    let pool = PgPoolOptions::new()
+        .max_connections(20)
+        .min_connections(2)
+        .acquire_timeout(std::time::Duration::from_secs(5))
+      .connect(url)
+      .await?;
+    Ok(Self { pool })
+  }
+
+pub async fn run_migrations(&self) -> anyhow::Result<()> {
+        sqlx::migrate!("./migrations")
+            .run(&self.pool)
+            .await?;
+        tracing::info!("migrations applied");
+        Ok(())
+    }
+}
+
+impl std::ops::Deref for Database {
+    type Target = PgPool;
+
+    fn deref(&self) -> &Self::Target {
+        &self.pool
+    }
+}
